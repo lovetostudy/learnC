cc545f8078ee399bf16b4aaebb6e0aadca2f654f
Kai Zhang <kai137497294@gmail.com>
2017-11-21 13:07:06 +0800

    problem in pushing project_13

--- homework/function/project_13/project_13.cpp
+++ homework/function/project_13/project_13.cpp
@@ -4,6 +4,21 @@
 
 int main()
 {
-	
+	int number;
+	int i;
+
+	printf("Enter numbers: ");
+	for(i = 0; i < 100; i++)
+	{
+		scanf("%d", &number);
+		if(number == 0)
+			break;
+		num = add(number);
+		if(num % 2 != 1)
+			printf("%5d", number);
+	}
+
+	printf("\n");
+
 	return 0;
 }
